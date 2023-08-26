0b376f049920376c461af7ab569350da0f80b252
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-26 22:39:36 +0300

    Animate about me section

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function About() {
   return (
-    <section
+    <motion.section
       className="py-5 sm:py-10 mt-5 sm:mt-10 "
       id="about-me"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, delay: 1 }}
+      transition={{
+        ease: "easeInOut",
+        duration: 0.7,
+        delay: 0.15,
+      }}
     >
       <div className="text-center">
         <h3 className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light ">About Me:</h3>
@@ -12,13 +20,13 @@ export default function About() {
           className="r mt-10 text-md md:text-xl"
           style={{ lineHeight: "2.5" }}
         >
-          I'm a Full Stack Web Developer, with profiency in:
+          I'm a Full Stack Web Developer, with proficiency in:
           <br />
           React, TypeScript, Node.js/Express, PHP/Laravel, and various databases.
           <br />
           I'm a fast learner and can learn and adapt to any technology required.
         </p>
       </div>
-    </section>
+    </motion.section>
   );
 }
